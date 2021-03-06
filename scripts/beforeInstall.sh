187dfaf4a426358c31e283a7265c3208ab6ba739
Mark Alas <mark@ad-group.com.au>
2021-03-05 22:18:57 -0800

    Updated beforeInstall Script

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -22,8 +22,8 @@ yum -y remove httpd* php*
 # Install PHP 7.4
 yum  -y install php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap,pdo}
 
-# Install Apache 2.4
-yum -y install httpd24
+# Install Apache
+yum -y install httpd
 
 # Allow URL rewrites
 sed -i 's#AllowOverride None#AllowOverride All#' /etc/httpd/conf/httpd.conf
